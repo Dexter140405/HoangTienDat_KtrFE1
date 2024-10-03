6f573c930fc2a267bf65c9309c7c2e834609bb77
dat67 <dat67@MSI>
2024-10-03 15:02:32 +0700

    Add project files.

---
+++ HoangTienDat_KtrFE1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HoangTienDat_KtrFE1.MvcApplication" Language="C#" %>
---
+++ HoangTienDat_KtrFE1/Views/Home/Myjs/JavaScript.js
@@ -0,0 +1,24 @@
+﻿ 
+    let subtotal = 8480000;
+    let shippingFee = 42000; 
+    let shippingPromotion = 30000; 
+    
+    function calculateTotal() { 
+        let total = subtotal + shippingFee - shippingPromotion;
+    document.getElementById('totalAmount').textContent = total.toLocaleString('vi-VN', {
+        style: 'currency',
+    currency: 'VND'
+        });
+    }
+    function updateShippingFee(fee) {
+        shippingFee = fee;
+    calculateTotal(); 
+    }
+    function updateShippingPromotion(promotion) {
+        shippingPromotion = promotion;
+    calculateTotal(); 
+    } 
+    window.onload = function() {
+        calculateTotal();
+    };
+
